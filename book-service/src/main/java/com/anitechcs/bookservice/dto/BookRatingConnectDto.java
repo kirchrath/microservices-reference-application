3e7fc3be72b5cd96145b20fdb237113c42c979fb
Chandra Kirchrath <chandra.kirchrath@linkmobility.com>
2021-03-13 19:52:14 +0100

    feat(connect-ratings): add wrapper dto for kafka connect payload handling

---
+++ book-service/src/main/java/com/anitechcs/bookservice/dto/BookRatingConnectDto.java
@@ -0,0 +1,10 @@
+package com.anitechcs.bookservice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookRatingConnectDto {
+    private BookRatingDto payload;
+}
